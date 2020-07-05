ba0effe08930f7b4fb51e9d011cea29b83890561
Raphael C. Almeida <raphael.carvalho@e-xyon.com.br>
2020-07-05 14:28:50 -0300

    Fixed UserID RMQ format

--- internal/models/scraping.go
+++ internal/models/scraping.go
@@ -1,10 +1,10 @@
 package models
 
 type ScrapingCredentials struct {
-	CEI *CEI `json:"cei"`
+	CEI *CEI `json:"cei" bson:"cei"`
 }
 
 type Scraping struct {
-	ID                  string              `json:"id"`
-	ScrapingCredentials ScrapingCredentials `json:"scraping_credentials"`
+	ID                  string              `json:"user_id" bson:"user_id"`
+	ScrapingCredentials ScrapingCredentials `json:"scraping_credentials" bson:"scraping_credentials"`
 }
